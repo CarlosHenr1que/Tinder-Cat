fd4125d689ede963314ed5bc8776e552d6b5d845
carlos.compasso <carlos.compasso@fleetcor.com.br>
2022-08-06 11:15:08 -0300

    faet: add get breeds request

--- src/screens/Cats/index.tsx
+++ src/screens/Cats/index.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SwiperDeck} from '../../components';
 
 import {Breed} from '../../models';
+import {requestGetBreedList} from '../../services/breed';
 import {Container} from './styles';
 
 const Cats: React.FC = () => {
-  const breeds: Breed[] = [
-    {
-      id: 'abys',
-      name: 'Abyssinian',
-      origin: 'Egypt',
-      image: {
-        id: 'abysimage',
-        url: 'https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg',
-      },
-    },
-  ];
+  const [breeds, setBreeds] = useState<Breed[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await requestGetBreedList({page: 0, limit: 10});
+      if (response) {
+        setBreeds(response);
+      }
+    })();
+  }, []);
+
   return (
     <Container>
       <SwiperDeck
