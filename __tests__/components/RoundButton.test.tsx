7494f57fa3aadae064ee035662bb422d3b553db9
carlos.compasso <carlos.compasso@fleetcor.com.br>
2022-08-06 09:56:21 -0300

    test: ensure render component correctly

---
+++ __tests__/components/RoundButton.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import {render} from '@testing-library/react-native';
+import RoundButton from '../../src/components/RoundButton';
+
+import {ThemeProvider} from 'styled-components/native';
+import theme from '../../src/styles/themes/default';
+import {ImageSourcePropType} from 'react-native';
+
+const makeSut = () => {
+  const onPressMock = jest.fn();
+  const image: ImageSourcePropType = {
+    uri: 'any_uri',
+  };
+
+  const sut = (
+    <ThemeProvider theme={theme}>
+      <RoundButton onPress={onPressMock} icon={image} />,
+    </ThemeProvider>
+  );
+
+  return {
+    sut,
+    onPressMock,
+    image,
+  };
+};
+
+describe('RoundButton', () => {
+  it('should render component correctly', () => {
+    const {image, sut} = makeSut();
+    const {getByTestId} = render(sut);
+
+    expect(getByTestId('round_button_icon').props).toHaveProperty(
+      'source',
+      image,
+    );
+  });
+});
