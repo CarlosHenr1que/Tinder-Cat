db7bedac1ff471b3b28e9980c800df85ac590123
carlos.compasso <carlos.compasso@fleetcor.com.br>
2022-08-06 11:23:46 -0300

    feat: add breeds stored pagination

--- src/screens/Cats/index.tsx
+++ src/screens/Cats/index.tsx
@@ -1,18 +1,63 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useEffect, useState} from 'react';
 import {SwiperDeck} from '../../components';
+
+import {usePagination} from '../../hooks/usePagination';
 
 import {Breed} from '../../models';
 import {requestGetBreedList} from '../../services/breed';
 import {Container} from './styles';
 
+const LIMIT = 10;
+const MINIMUM_SIZE = 1;
+
 const Cats: React.FC = () => {
   const [breeds, setBreeds] = useState<Breed[]>([]);
+  const {page, nextPage, getLastStoredPagination, saveLastItemID} =
+    usePagination({
+      pageKey: 'tinderCat@breedsLastPage',
+      lastItemKey: 'tinderCat@breedsLastBreedId',
+    });
+
+  const handleCardSwipe = (currentSize: number, swipedID: string) => {
+    saveLastItemID(swipedID);
+    if (currentSize === MINIMUM_SIZE) {
+      handleRequestBreeds(page + 1);
+      nextPage();
+    }
+  };
+
+  const handleRequestBreeds = async (currentPage: number) => {
+    const data = await requestGetBreedList({page: currentPage, limit: LIMIT});
+    if (data) {
+      setBreeds(previousState => {
+        if (previousState.length === 0) {
+          return data;
+        }
+        return [previousState[previousState.length - 1], ...data];
+      });
+    }
+  };
+
+  const handleRequestRecoverBreed = async (
+    currentPage: number,
+    lastBreedID: string | null,
+  ) => {
+    const data = await requestGetBreedList({page: currentPage, limit: LIMIT});
+
+    if (data) {
+      const lastBreedIndex = data.findIndex(item => item.id === lastBreedID);
+      setBreeds(data.slice(lastBreedIndex + 1));
+    }
+  };
 
   useEffect(() => {
     (async () => {
-      const response = await requestGetBreedList({page: 0, limit: 10});
-      if (response) {
-        setBreeds(response);
+      const {lastItemID, lastPage} = await getLastStoredPagination();
+      if (lastItemID || lastPage) {
+        handleRequestRecoverBreed(Number(lastPage), lastItemID);
+      } else {
+        handleRequestBreeds(page);
       }
     })();
   }, []);
@@ -21,7 +66,7 @@ const Cats: React.FC = () => {
     <Container>
       <SwiperDeck
         data={breeds}
-        onSwiped={() => {}}
+        onSwiped={handleCardSwipe}
         onLeftSwipe={() => {}}
         onRightSwipe={() => {}}
       />
