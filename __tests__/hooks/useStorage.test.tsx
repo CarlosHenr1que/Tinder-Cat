c5f2f8b6c20e4cdbc712def5e8cf7ce72ddfc158
carlos.compasso <carlos.compasso@fleetcor.com.br>
2022-08-10 22:16:39 -0300

    test: ensure useStorage store a value with async storage

---
+++ __tests__/hooks/useStorage.test.tsx
@@ -0,0 +1,18 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import {renderHook} from '@testing-library/react-native';
+import {useStorage} from '../../src/hooks/useStorage';
+
+const StorageMock = AsyncStorage as jest.Mocked<typeof AsyncStorage>;
+
+describe('Hook: useStorage', () => {
+  it('should store a value with async storage', async () => {
+    const {result} = renderHook(() => useStorage());
+    const id = 'any_id';
+    const value = 'any_value';
+
+    await result.current.setStoredValue(id, value);
+
+    expect(StorageMock.setItem).toHaveBeenCalledWith(id, value);
+  });
+});
