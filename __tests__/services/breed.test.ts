b91df3579e5492c8737cfd7a61990fbb606a6559
carlos.compasso <carlos.compasso@fleetcor.com.br>
2022-08-10 22:25:48 -0300

    test: ensure breed service throw a error when response is not 200

--- __tests__/services/breed.test.ts
+++ __tests__/services/breed.test.ts
@@ -1,11 +1,27 @@
 import {requestGetBreedList} from '../../src/services/breed';
 import {breeds} from '../../test/mocks/breeds';
 
+import {server} from '../../test/server/server';
+import {rest} from 'msw';
+import URL from '../../test/mocks/url';
+
 describe('Breed Service', () => {
   it('should get breed list', async () => {
     const data = await requestGetBreedList({page: 0, limit: 1});
 
     expect(data).toHaveLength(1);
     expect(data?.[0]).toEqual(breeds[0]);
+  });
+
+  it('should throw a error when response is not 200', async () => {
+    server.use(
+      rest.get(URL.BREEDS, (req, res, ctx) => {
+        return res.once(ctx.status(400));
+      }),
+    );
+
+    const data = requestGetBreedList({page: 0, limit: 1});
+
+    await expect(data).rejects.toThrow(new Error('could not load breeds'));
   });
 });
---
+++ test/mocks/url.ts
@@ -0,0 +1,6 @@
+import {API_URL} from '@env';
+
+export default {
+  VOTES: `${API_URL}/votes`,
+  BREEDS: `${API_URL}/breeds`,
+};
