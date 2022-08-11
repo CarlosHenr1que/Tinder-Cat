5cea1043e5d0f96682d1baa2aa67de1a3149c3c5
carlos.compasso <carlos.compasso@fleetcor.com.br>
2022-08-10 22:26:08 -0300

    test: ensure vote service throw a error when response is not 201

--- __tests__/services/vote.test.ts
+++ __tests__/services/vote.test.ts
@@ -1,5 +1,10 @@
 import {requestSendVote} from '../../src/services/vote';
 
+import {server} from '../../test/server/server';
+import {rest} from 'msw';
+
+import URL from '../../test/mocks/url';
+
 describe('Vote Service', () => {
   it('should post a vote', async () => {
     const data = await requestSendVote({
@@ -9,5 +14,21 @@ describe('Vote Service', () => {
     });
 
     expect(data.message).toBe('SUCCESS');
+  });
+
+  it('should throw a error when response is not 201', async () => {
+    server.use(
+      rest.post(URL.VOTES, (req, res, ctx) => {
+        return res.once(ctx.status(400));
+      }),
+    );
+
+    const data = requestSendVote({
+      image_id: 'any_id',
+      sub_id: 'any_sub_id',
+      value: 1,
+    });
+
+    await expect(data).rejects.toThrow(new Error('could not create a vote'));
   });
 });
